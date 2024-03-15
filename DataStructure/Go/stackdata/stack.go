d2c80f86ab5f79d0c465cae55b85845ac3018e70
yeelight <91862060+FeiNiaoBF@users.noreply.github.com>
2024-03-15 22:14:34 +0800

    Done: Stack is a common data structures

--- DataStructure/Go/stackdata/stack.go
+++ DataStructure/Go/stackdata/stack.go
@@ -1,7 +1,8 @@
 package stackdata
 
-/// Stack Data Structure in Golang
+import "fmt"
 
+/// Stack Data Structure in Golang
 // Stack Data Structure
 
 type node[T any] struct {
@@ -17,21 +18,21 @@ func newNode[T any](data T) *node[T] {
 }
 
 type Stack[T any] struct {
-	top  *node[T]
+	top *node[T]
 	size uint
 }
 
-func NewStack[T any]() *Stack[T]{
-	return &Stack[T] {
-		top: nil,
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		top:  nil,
 		size: 0,
 	}
 }
 
 // Public function of stack
 
-// Len get the stack length
-func (s *Stack[T]) Len() uint {
+// Size get the stack length
+func (s *Stack[T]) Size() uint {
 	return s.size
 }
 
@@ -42,4 +43,37 @@ func (s *Stack[T]) Push(data T) *Stack[T] {
 	s.top = newNode
 	s.size += 1
 	return s
+}
+
+// Pop pop the top node of the stack
+func (s *Stack[T]) Pop() T {
+	result := s.Peek()
+	s.top = s.top.next
+	s.size -= 1
+	return result
+}
+
+// IsEmpty return true if the stack is empty
+func (s *Stack[T]) IsEmpty() bool {
+	return s.size == 0
+}
+
+// Peek return the top node value
+func (s *Stack[T]) Peek() T {
+	if s.IsEmpty() {
+		var zeroValue T
+		return zeroValue
+	}
+	return s.top.data
+}
+
+// PrintStack print the stack
+func (s *Stack[T]) PrintStack() {
+	curr := s.top
+	fmt.Print("Stack: ")
+	for curr != nil {
+		fmt.Print(curr.data, "->")
+		curr = curr.next
+	}
+	fmt.Print("nil\n")
 }
